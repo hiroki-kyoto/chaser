76a65ef1f1683f0000b4c205149a05b1a8da58e8
Yamasaki Hiroki <yamasaki_hiroki_kyoto@yahoo.co.jp>
2017-10-06 23:26:14 +0800

    updated

---
+++ brain.py
@@ -0,0 +1,10 @@
+# brain.py
+# the engine for dealing with inputs,
+# mainly texts, audio and video 
+# may be implemented in later versions.
+
+class brain:
+	'''brain: learning how to understand text,
+	and how to generate text by internal states.
+	The core is a Recurrent neural network.
+	'''
